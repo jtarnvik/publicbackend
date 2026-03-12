e0432458eae49a9fd0b45739d3458335553945f9
jesper <jesper@tarnvik.com>
2026-03-12 13:50:42 +0100

    Initial commit with publicbackend

---
+++ src/main/java/com/tarnvik/publicbackend/PublicbackendApplication.java
@@ -0,0 +1,13 @@
+package com.tarnvik.publicbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarnvik/publicbackend/PublicbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarnvik.publicbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
