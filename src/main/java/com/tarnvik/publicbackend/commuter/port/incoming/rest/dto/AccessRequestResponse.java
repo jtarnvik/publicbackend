39ea0310a6234302339b3dd101162e7a49c2888e
jesper <jesper@tarnvik.com>
2026-03-25 19:22:30 +0100

    Added message showing

--- src/main/java/com/tarnvik/publicbackend/commuter/port/incoming/rest/dto/AccessRequestResponse.java
+++ src/main/java/com/tarnvik/publicbackend/commuter/port/incoming/rest/dto/AccessRequestResponse.java
@@ -1,3 +1,3 @@
 package com.tarnvik.publicbackend.commuter.port.incoming.rest.dto;
 
-public record AccessRequestResponse(Long id, String email, String name, String createDate) {}
+public record AccessRequestResponse(Long id, String email, String name, String createDate, String message) {}
